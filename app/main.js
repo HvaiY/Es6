2cc9f12e83287ecda8d3d9cec2d9f136a3f026b8
HvaiY <337646685@qq.com>
2019-03-18 23:33:08 +0800

    数组解构 更多

--- app/main.js
+++ app/main.js
@@ -5,3 +5,18 @@ console.log([a, b, c]);
 
 let [x, y, ...head] = ["a"];
 console.log([x, y, ...head]);
+// 部分解构也是可以成功的，未匹配到的就是undefined
+// 一边是数组一边不是 那么报错-- 不是可遍历的解构会报错iterator
+let [foo] = [123];
+//let [foo] = 1;
+//let [foo] = false;
+//let [foo] = NaN;
+//let [foo] = undefined;
+//let [foo] = null;
+//let [foo] = {};
+
+console.log([foo]);
+
+//Set 也可以解构赋值 也可以设置默认值
+let [z, v, n = 2] = new Set(["a", "b"]);
+console.log([z, v, n]);
